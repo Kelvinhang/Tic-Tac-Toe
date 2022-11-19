ed1f0f9f8f08087416918f7c0ad78ac1e2133a8a
Kelvinhang <hfyj77h47@gmail.com>
2022-11-19 18:19:51 +0800

    update setup gameboard

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,3 +1,18 @@
-function testing() {
-    document.getElementById("test").innerHTML = "Hello!";
+function setGameBoard() {
+    const boardObjectPrefab = new Array;
+    const gameBoard = document.querySelector(".gameboard")
+    for (let i=0; i<8; i++){
+        boardObjectPrefab.push("");
+    }
+
+    for (let i=0; i<=boardObjectPrefab.length; i++){
+        let boardObject = document.createElement("div");
+        boardObject.textContent = boardObjectPrefab[i];
+        boardObject.setAttribute("class", "gameboard_item");
+        boardObject.setAttribute("id", i);
+
+        gameBoard.appendChild(boardObject);
+    }
 }
+
+setGameBoard();
