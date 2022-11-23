9ec234efafd8d20810712144730baea39f935834
Kelvinhang <hfyj77h47@gmail.com>
2022-11-23 22:58:35 +0800

    add turn, win  update gameboard

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,7 +2,11 @@ function setGameBoard() {
     const boardObjectPrefab = new Array;
     const player1 = player("player1","O");
     const player2 = player("player2","X");
-    const gameBoard = document.querySelector(".gameboard")
+    const thisTurn = document.querySelector(".turn");
+    const gameBoard = document.querySelector(".gameboard");
+
+    thisTurn.textContent = player1.getName() + " turn";
+
     for (let i=0; i<9; i++){
         boardObjectPrefab.push("");
         let boardObject = document.createElement("div");
@@ -15,12 +19,14 @@ function setGameBoard() {
             turn == 0){
                 boardObject.textContent = player1.getSign();
                 turn++;
-                turn = turn%2;
+                turn = turn % 2;
+                thisTurn.textContent = player2.getName() + " turn";
             } else if (boardObject.textContent == "" &&
             turn == 1){
                 boardObject.textContent = player2.getSign();
                 turn++;
-                turn = turn%2;
+                turn = turn % 2;
+                thisTurn.textContent = player1.getName() + " turn";
             }
         })
         gameBoard.appendChild(boardObject);
@@ -31,7 +37,36 @@ function player(name,sign) {
     const getName = () => name;
     const getSign = () => sign;
 
+
     return {getName, getSign};
+}
+
+function win(){
+    let board = document.querySelectorAll(".gameboard_item");
+
+    for(let i = 0; i<9; i+=3){
+        if(board[i].innerHTML == board[i+1].innerHTML &&
+            board[i+1].innerHTML == board[i+2].innerHTML){
+                console.log("win");
+                break;
+            }
+    }
+
+    for(let i = 0; i<3; i++){
+        if(board[i].innerHTML == board[i+3].innerHTML &&
+            board[i+3].innerHTML == board[i+6].innerHTML){
+                console.log("win");
+                break;
+            }
+    }
+
+    if(board[0].innerHTML == board[4].innerHTML &&
+        board[4].innerHTML == board[8].innerHTML){
+            console.log("win");
+        } else if(board[2].innerHTML == board[4].innerHTML &&
+            board[4].innerHTML == board[6].innerHTML){
+                console.log("win");
+            }
 }
 
 setGameBoard();
