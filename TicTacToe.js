338aeabe8472aa8ec1e67b7607b7f5811ee689e6
Kelvinhang <hfyj77h47@gmail.com>
2022-11-25 14:47:54 +0800

    add resetbtn, project completed

--- TicTacToe.js
+++ TicTacToe.js
@@ -4,29 +4,90 @@ function setGameBoard() {
     const player2 = player("player2","X");
     const thisTurn = document.querySelector(".turn");
     const gameBoard = document.querySelector(".gameboard");
+    const reset = document.querySelector(".reset");
+    let won = false;
+    let turn = 0;
 
     thisTurn.textContent = player1.getName() + " turn";
+
+    let resetbtn = document.createElement("button");
+    resetbtn.textContent = "Reset";
+    resetbtn.addEventListener("click", (e) => {
+        let board = document.querySelectorAll(".gameboard_item");
+        for (let i = 0; i < 9; i++){
+            board[i].innerHTML = boardObjectPrefab[i];
+        }
+        turn = 0;
+        thisTurn.textContent = player1.getName() + " turn";
+    })
+
+    reset.appendChild(resetbtn);
+
+    function isWin(){
+        let board = document.querySelectorAll(".gameboard_item");
+    
+        for(let i = 0; i<9; i+=3){
+            if(board[i].innerHTML == board[i+1].innerHTML &&
+                board[i+1].innerHTML == board[i+2].innerHTML &&
+                board[i].innerHTML != ""){
+                    won = true;
+                    break;
+                }
+        }
+    
+        for(let i = 0; i<3; i++){
+            if(board[i].innerHTML == board[i+3].innerHTML &&
+                board[i+3].innerHTML == board[i+6].innerHTML &&
+                board[i].innerHTML != ""){
+                    won = true;
+                    break;
+                }
+        }
+    
+        if(board[0].innerHTML == board[4].innerHTML &&
+            board[4].innerHTML == board[8].innerHTML &&
+            board[0].innerHTML != ""){
+                won = true;
+            } else if(board[2].innerHTML == board[4].innerHTML &&
+                board[4].innerHTML == board[6].innerHTML &&
+                board[2].innerHTML != ""){
+                    won = true;
+                }
+    }
+
 
     for (let i=0; i<9; i++){
         boardObjectPrefab.push("");
         let boardObject = document.createElement("div");
-        var turn = 0;
         boardObject.textContent = boardObjectPrefab[i];
         boardObject.setAttribute("class", "gameboard_item");
         boardObject.setAttribute("id", i);
         boardObject.addEventListener("click", (e) =>{
             if (boardObject.textContent == "" &&
-            turn == 0){
+            turn == 0 &&
+            won == false){
                 boardObject.textContent = player1.getSign();
                 turn++;
                 turn = turn % 2;
-                thisTurn.textContent = player2.getName() + " turn";
+                isWin();
+                if (won == false){
+                    thisTurn.textContent = player2.getName() + " turn";
+                } else {
+                    thisTurn.textContent = player1.getName() + " won";
+                }
+                
             } else if (boardObject.textContent == "" &&
-            turn == 1){
+            turn == 1 &&
+            won == false){
                 boardObject.textContent = player2.getSign();
                 turn++;
                 turn = turn % 2;
-                thisTurn.textContent = player1.getName() + " turn";
+                isWin();
+                if (won == false){
+                    thisTurn.textContent = player1.getName() + " turn";
+                } else {
+                    thisTurn.textContent = player2.getName() + " won";
+                }
             }
         })
         gameBoard.appendChild(boardObject);
@@ -39,34 +100,6 @@ function player(name,sign) {
 
 
     return {getName, getSign};
-}
-
-function win(){
-    let board = document.querySelectorAll(".gameboard_item");
-
-    for(let i = 0; i<9; i+=3){
-        if(board[i].innerHTML == board[i+1].innerHTML &&
-            board[i+1].innerHTML == board[i+2].innerHTML){
-                console.log("win");
-                break;
-            }
-    }
-
-    for(let i = 0; i<3; i++){
-        if(board[i].innerHTML == board[i+3].innerHTML &&
-            board[i+3].innerHTML == board[i+6].innerHTML){
-                console.log("win");
-                break;
-            }
-    }
-
-    if(board[0].innerHTML == board[4].innerHTML &&
-        board[4].innerHTML == board[8].innerHTML){
-            console.log("win");
-        } else if(board[2].innerHTML == board[4].innerHTML &&
-            board[4].innerHTML == board[6].innerHTML){
-                console.log("win");
-            }
 }
 
 setGameBoard();
